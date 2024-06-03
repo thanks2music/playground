b59b9c54f86be6339eb9ea097b819178e36573c4
Yoshiyuki Ito <thanks2music@gmail.com>
2024-06-03 12:51:25 +0900

    Add static file export setting

--- nextjs/with-app/src/app/page.tsx
+++ nextjs/with-app/src/app/page.tsx
@@ -10,9 +10,9 @@ const RowdiesFont = Rowdies({
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={RowdiesFont.className}>Yoshiyuki Ito&apos;s Asobiba</h1>
+      <h1 className={RowdiesFont.className}>Yoshiyuki Ito&apos;s Playground</h1>
 
-      <div className={styles.description}>
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
@@ -97,7 +97,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
