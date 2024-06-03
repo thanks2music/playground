11e92e00b373ca94de591fec45dc0b9e30b274f3
Yoshiyuki Ito <thanks2music@gmail.com>
2024-06-03 21:10:54 +0900

    Add getting started hello.go

--- languages/golang/getting_started/hello.go
+++ languages/golang/getting_started/hello.go
@@ -1,7 +1,15 @@
+// NOTE: Tutorial URL https://go.dev/doc/tutorial/getting-started
+
+// メインパッケージを宣言
 package main
 
+// Go標準ライブラリパッケージの1つで人気パッケージとの事
 import "fmt"
 
+// pkg.go.devにあるquoteパッケージを呼び出す。
+import "rsc.io/quote"
+
 func main() {
-		fmt.Println("Hello, World!")
+		// rsc.io/quoteモジュールの「Go」関数を使用
+    fmt.Println(quote.Go())
 }
